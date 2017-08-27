090d1a3c098fdfb092c82ebd625049fbd26735f2
satoru <satorulogic@gmail.com>
2017-08-27 11:27:48 +0800

    Add MemcachedCache and documents

--- cachelper/remote.py
+++ cachelper/remote.py
@@ -1,7 +1,10 @@
 import inspect
 import functools
 
-from werkzeug.contrib.cache import RedisCache as _RedisCache
+from werkzeug.contrib.cache import (
+    RedisCache as _RedisCache,
+    MemcachedCache as _MemcachedCache,
+)
 
 
 __all__ = ['HelperMixin', 'RedisCache']
@@ -9,7 +12,30 @@ __all__ = ['HelperMixin', 'RedisCache']
 
 class HelperMixin(object):
 
+    '''A mixin class that add helpers to basic cache classes.
+
+    Basic cache classes are classes with the following methods:
+
+        - def get(self, key)
+        - def set(self, key, value, timeout=None)
+        - def delete(self, key)
+    '''
+
     def call(self, func, key, timeout=None):
+        '''Wraps a function call with cache.
+
+        Args:
+            func (function): the function to call.
+            key (str): the cache key for this call.
+            timeout (int): the cache timeout for the key (the
+                           unit of this parameter depends on
+                           the cache class you use, for example,
+                           if you use the classes from werkzeug,
+                           then timeout is in seconds.)
+
+        Returns:
+            The return value of calling func
+        '''
         result = self.get(key)
         if result is None:
             result = func()
@@ -17,6 +43,24 @@ class HelperMixin(object):
         return result
 
     def __call__(self, key_pattern, timeout=None):
+        '''Use the cache object as a decorator factory.
+
+        Args:
+            key_pattern (str): the key pattern to use for generating
+                               keys for caches of the decorated function
+            timeout (int): the cache timeout for the key
+
+        Returns:
+            a decorator that cache return values of the decorated functions
+            in keys generated based on the given key pattern
+            and the calling args.
+
+        Here's an example::
+
+            @cache('key-name-for-{id}')
+            def get_name(id):
+                return 'name' + id
+        '''
 
         def decorator(f):
 
@@ -35,7 +79,11 @@ class HelperMixin(object):
 
 
 class RedisCache(_RedisCache, HelperMixin):
-    pass
+    '''werkzeug.contrib.cache.RedisCache mixed with HelperMixin'''
+
+
+class MemcachedCache(_MemcachedCache, HelperMixin):
+    '''werkzeug.contrib.cache.MemcachedCache mixed with HelperMixin'''
 
 
 def make_key(key_pattern, func, args, kwargs):
