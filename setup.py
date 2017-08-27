f61dbc74c52aa7debdd8d09dd494a8780e85b024
satoru <satorulogic@gmail.com>
2017-08-27 20:30:14 +0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='cachelper',
-    version='0.1.0',
+    version='0.1.1',
     description='A collection of cache helpers',
     author='Satoru Logic',
     author_email='satorulogic@gmail.com',
