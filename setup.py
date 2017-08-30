a690a4c3cf3b9b533fe4ca88337703e917ebeed5
satoru <satorulogic@gmail.com>
2017-08-30 08:00:59 +0800

    Add more detail in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 from distutils.core import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 
 setup(
     name='cachelper',
     version='0.1.3',
     description='A collection of cache helpers',
+    long_description=long_description,
+    url='https://github.com/suzaku/cachelper',
+    license='MIT',
     author='Satoru Logic',
     author_email='satorulogic@gmail.com',
     packages=['cachelper'],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='cache',
     install_requires=[
         'Werkzeug>=0.9',
     ],
